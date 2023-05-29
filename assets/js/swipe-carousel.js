e45d3261486ebd1bf6fe23cb6e2c419ca23fdeed
Yaroslav Kashkarov <yaroslavkashkarov@gmail.com>
2023-05-29 19:19:32 +0200

    create project carousel JS update into classes

---
+++ assets/js/swipe-carousel.js
@@ -0,0 +1,34 @@
+import Carousel from "./carousel.js";
+
+class SwipeCarousel extends Carousel {
+  constructor( ...args ) {
+    super( ...args );
+  }
+
+  _initListeners() {
+    super._initListeners();
+    this.container.addEventListener( "touchstart", this.swipeStart.bind( this ) );
+    this.container.addEventListener( "mousedown", this.swipeStart.bind( this ) );
+    this.container.addEventListener( "touchend", this.swipeEnd.bind( this ) );
+    this.container.addEventListener( "mouseup", this.swipeEnd.bind( this ) );
+  }
+
+  swipeStart( e ) {
+    this.startPosX =
+      e instanceof MouseEvent
+        ? e.pageX //MouseEvent
+        : e.changedTouches[ 0 ].pageX; //TouchEvent
+  }
+
+  swipeEnd( e ) {
+    this.endPosX =
+      e instanceof MouseEvent
+        ? e.pageX //MouseEvent
+        : e.changedTouches[ 0 ].pageX; //TouchEvent
+
+    if ( this.endPosX - this.startPosX > 100 ) this.prevHandler();
+    if ( this.endPosX - this.startPosX < -100 ) this.nextHandler();
+  };
+}
+
+export default SwipeCarousel;
